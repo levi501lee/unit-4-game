635cc500ac17dcfca51a8b96cbf15f4353794831
levi501 <levi501lee@gmail.com>
2019-09-11 23:43:21 -0600

    game functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,88 +1,106 @@
+$(document).ready(function () {
 
-    var targetDiv = document.getElementById("empty-div");
-    targetDiv.textContent = "Welcome to the Taliban";
-
-var Numbers = ["50", "70", "83",]
-var startMoney = ["3", "7", "11", "13"]
-var currentMoney = 0;
-var wins = 0;
-var losses = 0;
-
-var targetNumber = Numbers[Math.floor(Math.random() * Numbers.length)];
-
-$("#number-to-guess").text(targetNumber);
-
-var pocketMoney = startMoney[Math.floor(Math.random() * startMoney.length)];
-
-$("#pocket-money").text(pocketMoney);
-// I am going to need a way to add to pocketMoney
-
-console.log(targetNumber)
-
-var counter = 0;
-
-var numberOptions = [10, 5, 3, 7];
-
-// This loop creates each crystal for each option, but I need it to be random still
-for (var i = 0; i < numberOptions.length; i++) {
-
-    // For each iteration, we will create an imageCrystal
-    var imageCrystal = $("<img>");
-
-    // First each crystal to have CSS, it will be given the class ".crystal-image".
-    imageCrystal.addClass("crystal-image");
+    var Numbers = ["50", "70", "83",]
+    var targetNumber = Numbers[Math.floor(Math.random() * Numbers.length)];
+    $("#target-number").text(targetNumber);
 
 
-    var imageType = ["assets/images/crystal1.jpg", "assets/images/crystal2.jpg", "assets/images/crystal3.jpg", "assets/images/crystal4.jpg"];
-    imageType.forEach(function() {
-    
-    };
-    // for (var j = 0; j < imageType.length; j++) {
-        // Each imageCrystal will be given a src link to the crystal image NEED THIS TO BE RANDOM MAYBE
-        imageCrystal.attr("src", imageType[j]);
+    var startMoney = ["3", "7", "11", "13"]
+    var pocketMoney = startMoney[Math.floor(Math.random() * startMoney.length)];
+    $("#pocket-money").text(pocketMoney);
+    // I am going to need a way to add to pocketMoney
+
+    var wins = 0;
+    var losses = 0;
+
+    var j1;
+    var j2;
+    var j3;
+    var j4;
+
+
+    console.log(targetNumber)
+
+    var counter = 0;
+    var crystalOptions = [10, 5, 3, 7];
+
+    var randomItem = crystalOptions[Math.floor(Math.random() * crystalOptions.length)];
+    // function crystalValues(crystalOptions) {
+    // for (i = 0; i < crystalOptions.length; i++)
+    {
+
+        $("#button-" + (i + 1)).attr("value", randomItem[i]);
+        console.log(this);
     }
-
-    // Each imageCrystal will be given a data attribute called data-crystalValue.
-    // This data attribute will be set equal to the array value.
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-
-    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-    $("#crystals").append(imageCrystal);
-}
-
-// This time, our click event applies to every single crystal on the page. Not just one.
-$(".crystal-image").on("click", function () {
-
-    // Determining the crystal's value requires us to extract the value from the data attribute.
-    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-    // We then add the crystalValue to the user's "counter" which is a global variable.
-    // Every click, from every crystal adds to the global counter.
-    counter += crystalValue;
-
-    $("button").on("click", function() {
-    if (currentMoney == Numbers) {
-
-        wins++;
-        console.log(currentMoney);
-        $("#totalNumber").html(currentMoney);
-        $("#wins").html("Wins: " + wins);
+    j1 = arr[0];
+    j2 = arr[1];
+    j3 = arr[2];
+    j4 = arr[3];
+    // This SHOULD assign values to crystals
 
 
-        setTimeout(function() {gameReset("YOU WIN!! Welcome to the Taliban!")}, 200);
+    //  This should reset everything
+    function gameReset(x) {
+
+        pickRandomNumber(rand);
+
+        pickRandomCrystals(crystals);
+
+        crystalValues(crystalOptions);
+
+        $("#pocket-money").html(pocketMoney);
+
+        alert(x);
     }
+    // end of reset game
 
-    else if (currentMoney > Numbers){
+    // The game settings
 
-        losses++;
-        $("#totalNumber").html(currentMoney);
-        $("#losses").html("Losses: " + losses);
+    $("#button-1").on("click", function () {
 
-        setTimeout(function() {gameReset("INFIDEL! You stole too much! YOU LOSE!")}, 200);
-    }
+        pocketMoney += j1;
+        $("#pocket-money").html(pocketMoney);
+    });
+
+    $("#button-2").on("click", function () {
+
+        pocketMoney += j2;
+        $("#pocket-money").html(pocketMoney);
+    });
+
+    $("#button-3").on("click", function () {
+
+        pocketMoney += j3;
+        $("#pocket-money").html(pocketMoney);
+    });
+
+    $("#button-4").on("click", function () {
+
+        pocketMoney += j4;
+        $("#pocket-money").html(pocketMoney);
+    });
+
+
+    $("button").on("click", function () {
+        if (pocketMoney == Numbers) {
+
+            wins++;
+            console.log(pocketMoney);
+            $("#pocket-money").html(pocketMoney);
+            $("#wins").html("Wins: " + wins);
+
+
+            setTimeout(function () { gameReset("YOU WIN!! Welcome to the Taliban!") }, 200);
+        }
+
+        else if (pocketMoney > Numbers) {
+
+            losses++;
+            $("#pocket-money").html(pocketMoney);
+            $("#losses").html("Losses: " + losses);
+
+            setTimeout(function () { gameReset("INFIDEL! You stole too much! YOU LOSE!") }, 200);
+        }
+    });
+
 });
